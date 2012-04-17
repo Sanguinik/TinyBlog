8af4f8164958a5606bcc01fe0b73475037e97eea
Marlene <Knoche.Marlene@gmx.de>
2012-04-17 18:23:03 +0200

    wrote some testcases and added a mock implementation for the articleDao

---
+++ TinyBlog/src/main/java/de/hszg/tinyblog/service/ArticleDaoMock.java
@@ -0,0 +1,51 @@
+package de.hszg.tinyblog.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hszg.tinyblog.model.Article;
+
+public class ArticleDaoMock implements ArticleDao {
+	
+	private List<Article> articles;
+	private Article article;
+	
+	public ArticleDaoMock(){
+		articles = new ArrayList<Article>();
+	}
+
+	@Override
+	public boolean addArticle(Article article) {	
+		return articles.add(article);
+	}
+
+	@Override
+	public boolean removeArticle(Article article) {
+		return articles.remove(article);
+	}
+
+	@Override
+	public Article setArticle(Article article) {
+		
+		return articles.set(0, article);
+	}
+
+	@Override
+	public Article findArticleById(long id) {
+	
+		for(int i = 0; i<=articles.size(); i++){
+			long foundId = articles.get(i).getId();
+			if(foundId == id){
+				return articles.get(i);
+			}
+			
+		}
+		return null;
+	}
+
+	@Override
+	public List<Article> findAllArticles() {
+		return articles;
+	}
+
+}
--- TinyBlog/src/test/java/de/hszg/tinyblog/service/ArticleDaoTest.java
+++ TinyBlog/src/test/java/de/hszg/tinyblog/service/ArticleDaoTest.java
@@ -1,31 +1,69 @@
 package de.hszg.tinyblog.service;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import de.hszg.tinyblog.model.Article;
+import de.hszg.tinyblog.model.User;
+
 public class ArticleDaoTest {
+	
+	private static final String MY_CONTENT = "My content.";
+	private static final String MY_TITLE = "My title";
+	private static final String MY_NEW_CONTENT = "My new content";
+	private static final String MY_NEW_TITLE = "My new title";
+	private ArticleDao articleDao;
+	private User user = new User("Marlene","secret","marlene@example.org");
+	
+	@Before
+	public void setUp(){
+		articleDao = new ArticleDaoMock();
+	}
 
 	@Test
-	public void testAddArticle() {
+	public void testAddAndRemoveArticle() {
+		Article article = new Article(MY_TITLE, MY_CONTENT, user);
+		assertTrue(articleDao.addArticle(article));
+		
+		List<Article> articles = articleDao.findAllArticles();
+		assertTrue(articles.contains(article));
+		
+		assertTrue(articleDao.removeArticle(article));
+		assertFalse(articles.contains(article));
 		
 	}
 	
+
 	@Test
-	public void testRemoveArticle(){
+	public void testAddAndEditArticle(){
+		Article article = new Article(MY_TITLE, MY_CONTENT, user);
+		assertTrue(articleDao.addArticle(article));
+		assertEquals(MY_TITLE, article.getTitle());
+		assertEquals(MY_CONTENT, article.getContent());
+		
+		article.setTitle(MY_NEW_TITLE);
+		article.setContent(MY_NEW_CONTENT);
+	
+		articleDao.setArticle(article);
+		
+		assertEquals(MY_NEW_TITLE, article.getTitle());
+		assertEquals(MY_NEW_CONTENT, article.getContent());
+		
 		
 	}
 
-	@Test
-	public void testSaveArticle(){
-		
-	}
-	
 	@Test
 	public void testFindArticleById(){
+		Article article = new Article(MY_TITLE, MY_CONTENT, user);
+		assertTrue(articleDao.addArticle(article));
+		long id = article.getId();
+		Article articleById = articleDao.findArticleById(id);
+		assertEquals(articleById, article);
 		
 	}
-	
-	@Test
-	public void testFindAllArticles(){
-		
-	}
+
 }
